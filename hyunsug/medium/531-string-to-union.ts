afef592738d489329c256278f185f5b2db6f609a
LucetTin5 <lucettin5@gmail.com>
2025-02-11 16:33:05 +0900

    [medium] 531-string-to-union

--- hyunsug/medium/531-string-to-union.ts
+++ hyunsug/medium/531-string-to-union.ts
@@ -1,9 +1,6 @@
-type StringToUnion<
-  T extends string,
-  U extends string = never
-> = T extends `${infer First}${infer Rest}`
-  ? First | StringToUnion<Rest, U>
-  : U;
+type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}`
+  ? First | StringToUnion<Rest>
+  : never;
 
 type Test = "123";
 type Result = StringToUnion<Test>; // expected to be "1" | "2" | "3"
