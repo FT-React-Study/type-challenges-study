f9eda3abbdaf754890224d902be2c6d2133cc375
LucetTin5 <lucettin5@gmail.com>
2025-04-16 21:34:28 +0900

    fix: deep-mutable 옮기는 중에 빠진 부분 추가

--- hyunsug/medium/17973-deep-mutable.ts
+++ hyunsug/medium/17973-deep-mutable.ts
@@ -1,5 +1,7 @@
 type DeepMutable<T extends { [key: keyof any]: any }> = {
-  -readonly [key in keyof T]: T[key] extends readonly unknown[]
+  -readonly [key in keyof T]: T[key] extends (...args: any[]) => any
+    ? T[key]
+    : T[key] extends readonly unknown[]
     ? DeepMutable<T[key]>
     : T[key] extends { [key: keyof any]: any }
     ? DeepMutable<T[key]>
