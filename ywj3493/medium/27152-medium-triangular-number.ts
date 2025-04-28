48093c0a62a256dabdcc51d9478fd98a8dd74218
ywj3493 <ywj3493@naver.com>
2025-04-28 10:37:07 +0900

    [medium] 27152 - triangular-number

---
+++ ywj3493/medium/27152-medium-triangular-number.ts
@@ -0,0 +1,47 @@
+/*
+  27152 - Triangular number
+  -------
+  by null (@aswinsvijay) #보통 #tuple #array #math
+
+  ### 질문
+
+  Given a number N, find the Nth triangular number, i.e. `1 + 2 + 3 + ... + N`
+
+  > GitHub에서 보기: https://tsch.js.org/27152/ko
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+// type Triangular<N extends number, I extends never[] = [], J extends never[] = [], C extends never[] = []> = 
+//   I['length'] extends N ?
+//     C['length'] :
+//     J['length'] extends [...I, never]['length'] ?
+//       Triangular<N, [...I, never], J, [...C, ...J]>:
+//       Triangular<N, I, [...J, never], C>;
+
+type Triangular<N extends number, I extends never[] = [], C extends never[] = []> = 
+  I['length'] extends N ?
+      C['length'] :
+      Triangular<N, [...I, never], [...C, ...[...I, never]]> ;
+
+type Test = Triangular<0>;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Triangular<0>, 0>>,
+  Expect<Equal<Triangular<1>, 1>>,
+  Expect<Equal<Triangular<3>, 6>>,
+  Expect<Equal<Triangular<10>, 55>>,
+  Expect<Equal<Triangular<20>, 210>>,
+  Expect<Equal<Triangular<55>, 1540>>,
+  Expect<Equal<Triangular<100>, 5050>>,
+]
+
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/27152/answer/ko
+  > 정답 보기: https://tsch.js.org/27152/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
