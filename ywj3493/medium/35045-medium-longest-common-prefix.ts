93c610dc4b5a3e067de19f2d0bf0e51580678f06
ywj3493 <ywj3493@naver.com>
2025-05-14 17:54:36 +0900

    [medium] 35045 - longest-common-prefix

---
+++ ywj3493/medium/35045-medium-longest-common-prefix.ts
@@ -0,0 +1,63 @@
+/*
+  35045 - Longest Common Prefix
+  -------
+  by Tom Cleary (@thomcleary) #보통
+
+  ### 질문
+
+  ### Longest Common Prefix
+
+  Write a type, `LongestCommonPrefix` that returns the longest common prefix string amongst a tuple of strings.
+
+  If there is no common prefix, return an empty string `""`.
+
+  ```ts
+  type Common = LongestCommonPrefix<["flower", "flow", "flight"]>
+  //   ?^ "fl"
+
+  type Uncommon = LongestCommonPrefix<["dog", "racecar", "race"]>
+  //   ?^ ""
+  ```
+  Inspired by [LeetCode 14. Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/description/)
+
+  > GitHub에서 보기: https://tsch.js.org/35045/ko
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+type LongestCommonPrefix<T extends string[], Std extends string = T[0], P extends string = '', Answer extends string = ''> = 
+  Std extends `${infer First}${infer Rest}` ?
+    // 기준 문자가 '' 이 아닐 때,
+    T[number] extends `${P}${string}` ?
+      LongestCommonPrefix<T, Rest, `${P}${First}`, P> :
+      Answer :
+    // 기준 문자가 '' 일 때,
+    T[number] extends `${P}${string}` ?
+      P :
+      '';
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LongestCommonPrefix<['flower', 'flow', 'flight']>, 'fl'>>,
+  Expect<Equal<LongestCommonPrefix<['dog', 'racecar', 'race']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', '', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', '', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', 'a', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', '', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', 'a', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', '', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', 'a', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', 'a', 'a']>, 'a'>>,
+  Expect<Equal<LongestCommonPrefix<['abc', 'abcd', 'abcde']>, 'abc'>>,
+  Expect<Equal<LongestCommonPrefix<[' ', ' ', ' ']>, ' '>>,
+  Expect<Equal<LongestCommonPrefix<['type-challenges', 'type-hero', 'typescript']>, 'type'>>,
+]
+
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/35045/answer/ko
+  > 정답 보기: https://tsch.js.org/35045/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
